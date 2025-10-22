0435faf8c6eca16a247a03af950e66db1a1633d0
João Paulo Medeiros Rodrigues <jp.estudos17@gmail.com>
2025-10-22 19:27:03 -0300

    Add files via upload

---
+++ 1140 - Flores Florecem da França.py
@@ -0,0 +1,14 @@
+while True:
+    frase = list(map(str, input().upper().split()))
+    if frase[0][0] == "*":
+        break
+
+    boole = True
+    aux = frase[0][0]
+    for letra in frase:
+        if letra[0] != aux:
+            boole = False
+    if boole:
+        print("Y")
+    else:
+        print("N")
