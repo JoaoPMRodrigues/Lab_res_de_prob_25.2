d3819e5f91eb7ca9ec6b6937a13ef2d93f9a8356
João Paulo Medeiros Rodrigues <jp.estudos17@gmail.com>
2025-11-21 01:31:06 -0300

    Add files via upload

--- 2227 - Aeroporto.py
+++ 2227 - Aeroporto.py
@@ -1,24 +1,23 @@
-#5% Presentation Error
-contador = 1
-aeroportos = []
+t = 1
+
 while True:
-    a, v = map(int, input().split())
-    if a == v == 0:
+    A, V = map(int, input().split())
+    if A == 0 and V == 0:
         break
 
-    grau = [0] * (a + 1)
+    grau = [0] * (A + 1)
 
-    for _ in range(v):
+    for _ in range(V):
         x, y = map(int, input().split())
         grau[x] += 1
         grau[y] += 1
 
     maior = max(grau[1:])
-    resposta = [str(i) for i in range(1, a + 1) if grau[i] == maior]
-    aeroportos.append(resposta[:])
+    
+    print(f"Teste {t}")
+    for i in range(1, A + 1):
+        if grau[i] == maior:
+            print(i, end=" ")
+    print("\n")
 
-for i in range(len(aeroportos)):
-    print(f"Teste {contador}")
-    print(" ".join(aeroportos[i]))
-    print()
-    contador += 1
+    t += 1
